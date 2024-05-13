274da9b039a5a79296cfef3998f1704bd9f130b5
DEV <dev@step.rivne>
2024-05-13 20:40:57 +0300

    skeleton for home page pizzas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Layout from './components/Layout';
+import Home from './components/Home';
 import Pizzas from './components/Pizzas';
 import CreateForm from './components/CreateForm';
 
@@ -9,7 +10,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<p>Home</p>} />
+          <Route index element={<Home />} />
           <Route path="pizzas" element={<Pizzas/>}/>
           <Route path="pizzas/create" element={<CreateForm />} />
           <Route path="beverages" element={<p>Beverages</p>} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Card, Flex  } from 'antd';
+import { pizzasService } from '../server/pizzas';
 
 export default function Home() {
+    const { Meta } = Card;
+
+    const [pizzas, setPizzas] = useState([]);
+
+    const loadPizzas = async () => {
+        const response = await pizzasService.get();
+        const items = response.data;
+
+        setPizzas(response.data);
+    }
+    
+    useEffect(() => {
+        loadPizzas();
+    }, []);
+
+    const App = () => (
+      <Card
+        hoverable
+        style={{
+          width: 240,
+        }}
+        cover={<img alt="pizza" src="" />}
+      >
+        <Meta title="" description="" />
+      </Card>
+    );
+
     return (
-        <div>Home</div>
+        <Flex wrap gap="small" dataSource={pizzas}>
+          {Array.from({length: 24,},
+            (_, i) => (
+                <div>{App()}</div>
+            ),
+          )}
+        </Flex>
     )
 }
+
